2872f87f4648e0edb63e58a9be7065138033193a
Ruslan <157638442+Ruslan-lion@users.noreply.github.com>
2024-04-21 16:35:19 +0300

    Delete src directory

--- src/MyFunctionalComponent.js
+++
@@ -1,9 +0,0 @@
-const MyFunctionalComponent = () => {
-  return (
-    <div className = "my-func">
-      Вітаю! Це мій функціональний компонент для виконання домашнього завдання.
-    </div>
-  );
-}
-
-export default MyFunctionalComponent;
